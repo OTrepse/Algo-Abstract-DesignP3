a2f8dd256c649333524d5ae06762f48919236339
OTrepse <rjesperto4@gmail.com>
2026-04-03 21:05:04 -0400

    start of main

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int A;
+    cin >> A;
+
+    unordered_map<char, int> count;
+    for (int i = 0; i < A; i++) {
+        char c;
+        int v;
+        cin >> c >> v;
+        count[c] = v;
+    }
+
+
+
+
+
+
+
+
+}
